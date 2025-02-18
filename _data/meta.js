80cf136d1f6617e601d12a38e93cfd922bcb3e51
Reuben L. Lillie <reubenlillie@gmail.com>
2025-02-18 09:37:11 -0600

    Restyle site title without periods 📏

--- _data/meta.js
+++ _data/meta.js
@@ -11,9 +11,9 @@
  */
 export default {
   baseUrl: 'https://earnsavegive.co', // No trailing slash
-  description: 'Earn Save Give offers tips and strategies to help you earn more, save smarter, and give back with purpose. Explore practical advice on credit cards, budgeting, investing, and charitable giving to build wealth and live a meaningful life. Take control of your finances today—whether you’re just starting out or looking to optimize every dollar. Let’s put your money to work.',
+  description: 'EarnSaveGive shares tips and strategies to help you earn more, save smarter, and give back with purpose. Explore practical advice on credit cards, budgeting, investing, and charitable giving to build wealth and live a meaningful life. Life isn’t about how much you have, but what you do with it. Take control of your finances today—whether you’re just starting out or looking to optimize every dollar. Let’s put your money to work.',
   madeWith: 'Made with 💚 from Chicago.',
-  title: 'Earn. Save. Give.',
+  title: 'EarnSaveGive',
   tagline: 'All You Can and More',
   locale: {
     code: 'en-US',
