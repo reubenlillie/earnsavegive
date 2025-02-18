9d31b21192e0d2b6cdad7c75994308fc7357d046
Reuben L. Lillie <reubenlillie@gmail.com>
2025-02-18 14:32:37 -0600

    Revise meta.disclaimer for clarity and concision 📝

--- _data/meta.js
+++ _data/meta.js
@@ -14,7 +14,7 @@ export default {
   description: 'EarnSaveGive shares tips and strategies to help you earn more, save smarter, and give back with purpose. Explore practical advice on credit cards, budgeting, investing, and charitable giving to build wealth and live a meaningful life. Life isn’t about how much you have, but what you do with it. Take control of your finances today—whether you’re just starting out or looking to optimize every dollar. Let’s put your money to work.',
   disclaimer: {
     heading: 'Our Promise to You',
-    text: 'At EarnSaveGive, we’re dedicated to helping you earn more, save smarter, and give back with purpose. Some of the links on our site may earn us a commission if you apply for a product or service. This compensation, however, does not influence our reviews or recommendations. We prioritize transparency so you can rely on our perspective as you take charge of your financial future.'
+    text: 'At EarnSaveGive, we’re dedicated to helping you earn more, save smarter, and give back with purpose. Some of the links on our site may earn us a commission if you apply for a product or service. This compensation, however, does not influence our reviews or recommendations. We value transparency so you can take charge of your financial future with confidence.'
   },
   madeWith: 'Made with 💚 from Chicago.',
   title: 'EarnSaveGive',
