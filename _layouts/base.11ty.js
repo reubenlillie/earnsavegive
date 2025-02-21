863d479e74cc722ea653a42aecbf31e8f0d29aa8
Reuben L. Lillie <reubenlillie@gmail.com>
2025-02-20 18:07:51 -0600

    Move font declarations to css/fonts.css

--- _layouts/base.11ty.js
+++ _layouts/base.11ty.js
@@ -28,50 +28,9 @@ export async function render(data) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="${meta.description}">
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💰</text></svg>">
-    <style>
-      /**
-       * Add Metropolis font by Chris Simpson
-       * @see {@link https://github.com/dw5/metropolis Metropolis on GitHub}
-       */
-      @font-face {
-        font-family: Metropolis;
-        src:
-          local(Metropolis),
-          url('/fonts/metropolis/Metropolis-Regular.woff2') format('woff2');
-        font-weight: 400;
-        font-style: normal;
-        font-display: swap;
-      }
-      @font-face {
-        font-family: Metropolis;
-        src:
-          local(Metropolis),
-          url('/fonts/metropolis/Metropolis-Bold.woff2') format('woff2');
-        font-weight: 700;
-        font-display: swap;
-      }
-      @font-face {
-        font-family: Metropolis;
-        src:
-          local(Metropolis),
-          url('/fonts/metropolis/Metropolis-RegularItalic.woff2') format('woff2');
-        font-weight: 400;
-        font-style: italic;
-        font-display: swap;
-      }
-      @font-face {
-        font-family: Metropolis;
-        src:
-          local(Metropolis),
-          url('/fonts/metropolis/Metropolis-BoldItalic.woff2') format('woff2');
-        font-weight: 700;
-        font-style: italic;
-        font-display: swap;
-      }
-      body {
-        font-family: Metropolis, sans-serif;
-      }
-    </style>
+    <style>${this.fileExists('css/fonts.css')
+      ? this.fileToString('css/fonts.css')
+      : '<!--No file at css/fonts.css-->'}</style>
   </head>
   <body>
     <header>
