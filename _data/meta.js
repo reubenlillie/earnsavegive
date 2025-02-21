cd1292eb062f00ed0df954cf29450a7c6b19cc3d
Reuben L. Lillie <reubenlillie@gmail.com>
2025-02-20 18:17:23 -0600

    Revise meta description, disclaimer, and title for consistency 📝

--- _data/meta.js
+++ _data/meta.js
@@ -11,14 +11,14 @@
  */
 export default {
   baseUrl: 'https://earnsavegive.co', // No trailing slash
-  description: 'EarnSaveGive shares tips and strategies to help you earn more, save smarter, and give back with purpose. Explore practical advice on credit cards, budgeting, investing, and charitable giving to build wealth and live a meaningful life. Life isn’t about how much you have, but what you do with it. Take control of your finances today—whether you’re just starting out or looking to optimize every dollar. Let’s put your money to work.',
+  description: 'Earn Save Give shares tips and strategies to help you earn more, save smarter, and give back with purpose. Explore practical advice on credit cards, budgeting, investing, and charitable giving to build wealth and live a meaningful life. Life isn’t about how much you have, but what you do with it. Take charge of your finances today—whether you’re just starting out or looking to optimize every dollar. Let’s put your money to work.',
   disclaimer: {
     heading: 'Our Promise to You',
-    text: 'At EarnSaveGive, we’re dedicated to helping you earn more, save smarter, and give back with purpose. Some of the links on our site may earn us a commission if you apply for a product or service. This compensation, however, does not influence our reviews or recommendations. We value transparency so you can make financial decisions with confidence.'
+    text: 'At Earn Save Give, we’re dedicated to helping you earn more, save smarter, and give back with purpose. Some of the links on our site may earn us a commission if you apply for a product or service. This compensation, however, does not influence our reviews or recommendations. We value transparency so you can make financial decisions with confidence.'
   },
   madeWith: 'Made with 💚 from Chicago.',
-  title: 'EarnSaveGive',
-  tagline: 'All You Can and More',
+  title: 'Earn Save Give',
+  tagline: 'All you can and more',
   locale: {
     code: 'en-US',
     dateOptions: {
